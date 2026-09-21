0a5a8458657d87f6014ddfc067290edea349a1d4
Zohaib Aslam <iamzohaib777@gmail.com>
2026-09-21 20:51:52 +0530

    fix 2

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -4,7 +4,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL:
-    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ??
+    process.env.NEXT_BETTER_AUTH_URL ??
     (typeof window !== "undefined" ? window.location.origin : undefined),
 });
 
